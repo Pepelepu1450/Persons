e31e0dc5e779b5ebcedb5ba72d996421c7f783e8
pepe <jose.vacaslocas@gmail.com>
2020-02-19 11:55:48 -0700

    se crearon personas orientado en objetos

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Persons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Jhoan = new Person ("Jhoan Smith");
+            Jhoan.SayHello();
+
+            Person Jhoana = new Person ("Jhoana Smith");
+            Jhoana.SayHello();
+
+            Console.WriteLine(Person.bloodColor);
+
+            Person.PrintRace();
+        }
+    }
+}
